fe4bed085c4e2960198e520ff4b3220d7058969d
victini-yang <826595923@qq.com>
2024-06-21 13:39:07 +0800

    云平台开发

---
+++ yang-yuzi-generator-web-backend/src/main/java/com/yupi/web/mapper/GeneratorMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.web.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.web.model.entity.Generator;
+
+/**
+ * @author 82659
+ * @description 针对表【generator(代码生成器)】的数据库操作Mapper
+ * @createDate 2024-06-19 20:28:42
+ * @Entity com.yupi.web.model.entity.Generator
+ */
+public interface GeneratorMapper extends BaseMapper<Generator> {
+
+}
+
+
+
+
---
+++ yang-yuzi-generator-web-backend/src/main/java/com/yupi/web/meta/Meta.java
@@ -0,0 +1,73 @@
+package com.yupi.web.meta;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @Author Victiny
+ * @Version 1.0
+ * @Date create in 2024/6/2 10:27
+ * 通过GsonFormatPlus插件读取Json文件alt + insert生成
+ */
+@NoArgsConstructor
+@Data
+public class Meta {
+
+    private String name;
+    private String description;
+    private String basePackage;
+    private String version;
+    private String author;
+    private String createTime;
+    private FileConfig fileConfig;
+    private ModelConfig modelConfig;
+
+    @NoArgsConstructor
+    @Data
+    public static class FileConfig {
+        private String inputRootPath;
+        private String outputRootPath;
+        private String sourceRootPath;
+        private String type;
+        private List<FileInfo> files;
+
+        @NoArgsConstructor
+        @Data
+        public static class FileInfo {
+            private String inputPath;
+            private String outputPath;
+            private String type;
+            private String generateType;
+            private String condition;
+            private String groupKey;
+            private String groupName;
+            private List<FileInfo> files;
+        }
+    }
+
+    @NoArgsConstructor
+    @Data
+    public static class ModelConfig {
+        private List<ModelInfo> models;
+
+        @NoArgsConstructor
+        @Data
+        public static class ModelInfo {
+            private String fieldName;
+            private String type;
+            private String description;
+            private Object defaultValue;
+            private String abbr;
+            private String groupKey;
+            private String groupName;
+            private List<ModelInfo> models;
+            private String condition;
+
+            // 中间参数
+            // 该分组下所有参数拼接字符串
+            private String allArgsStr;
+        }
+    }
+}
